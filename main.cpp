ec945d249cbdff5944316f475b1b8623bb65ad60
FlyingMinion10 <zepedajuanfe9@gmail.com>
2024-11-07 20:51:08 -0600

    Version 1 (Viernes) Funcionale

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
+int probabilidades[6] = {5,10,15,30,20,15};
+
 void darBienvenida(string clavadiststa_1, string pais, int year) {
-    cout << "Bienvenido a mi programa" << clavadiststa_1 << endl;
-    cout << "País: " << pais << " Año: " << year << endl;
+    cout << clavadiststa_1 << " "<< year << " -- " << pais << endl;
 }
 
 int generarNum1al100() {
@@ -14,75 +17,117 @@ float generarDificultadAleatoria(int n1, int n2) {
     return rand()%(n2-n1+1) + n1;
 }
 
+void indiceDeDesviacion(int x) {
+    // Asegurarse de que x no sea mayor que el tamaño del array
+    if (x > 6) {
+        x = 6;
+    }
+
+    for (int i = 0; i < x; ++i) {
+        // Guardar el primer elemento
+        int temp = probabilidades[0];
+
+        // Encontrar la posición correcta para colocar el elemento guardado
+        int j = 1;
+        while (j < 6 && probabilidades[j] > temp) {
+            probabilidades[j - 1] = probabilidades[j];
+            j++;
+        }
+        probabilidades[j - 1] = temp;
+    }
+}
+
+
+float generarDificultad() {
+    float dificultad;
+    int probabilidadDif = generarNum1al100();
+
+    if (probabilidadDif <= 5) {
+        dificultad = generarDificultadAleatoria(14, 19);
+    } else if (probabilidadDif <= 15) {
+        dificultad = generarDificultadAleatoria(20, 25);
+        indiceDeDesviacion(1);
+    } else if (probabilidadDif <= 30) {
+        dificultad = generarDificultadAleatoria(25, 30);
+        indiceDeDesviacion(1);
+    } else if (probabilidadDif <= 60) {
+        dificultad = generarDificultadAleatoria(30, 35);
+        indiceDeDesviacion(2);
+    } else if (probabilidadDif <= 80) {
+        dificultad = generarDificultadAleatoria(35, 40);
+        indiceDeDesviacion(2);
+    } else if (probabilidadDif <= 95) {
+        dificultad = generarDificultadAleatoria(40, 45);
+        indiceDeDesviacion(3);
+    } else {
+        dificultad = generarDificultadAleatoria(45, 47);
+        indiceDeDesviacion(3);
+    }
+    return dificultad;
+}
+
 float generarCalificacionAleatoria(int n1, int n2) {
     return rand()%(n2-n1+1) + n1;
 }
 
-int main() {
 
+int main() {
+    cout << "Bienvenido a mi programa" << endl;
+    
     srand(time(0));
-    string clavadiststa_1 = "Alejandra Orozco";
-    string pais = "Mexico";
-    int year = 2006;
+    string clavadiststa[7] = {"Alejandra Orozco", "Participant2", "Participant3", "Participant4", "Participant5", "Participant6", "Participant7"};
+    string pais[7] = {"Mexico", "Country2", "Country3", "Country4", "Country5", "Country6", "Country7"};
+    int year[7] = {2006, 2007, 2008, 2009, 2010, 2011, 2012};
     // Array de calificaciones
     float calificaciones[7];
 
-
-    darBienvenida(clavadiststa_1, pais, year);
+    darBienvenida(clavadiststa[0], pais[0], year[0]);
     
-    int probabilidadDif = generarNum1al100();
+    float dificultad = generarDificultad();
+    
     int probabilidadCal = generarNum1al100();
-    
-    float dificultad;
     float calificacion;
 
-    if (probabilidadDif <= 5) {
-        dificultad = generarDificultadAleatoria(14, 19);
-        probabilidadCal = 0;
-    } else if (probabilidadDif <= 15) {
-        dificultad = generarDificultadAleatoria(20, 25);
-        probabilidadCal = -5;
-    } else if (probabilidadDif <= 30) {
-        dificultad = generarDificultadAleatoria(25, 30);
-        probabilidadCal = -10;
-    } else if (probabilidadDif <= 60) {
-        dificultad = generarDificultadAleatoria(30, 35);
-        probabilidadCal = -15;
-    } else if (probabilidadDif <= 80) {
-        dificultad = generarDificultadAleatoria(35, 40);
-        probabilidadCal = -20;
-    } else if (probabilidadDif <= 95) {
-        dificultad = generarDificultadAleatoria(40, 45);
-        probabilidadCal = -25;
-    } else {
-        dificultad = generarDificultadAleatoria(45, 47);
-        probabilidadCal = -30;
+    
+
+    int probabilidadesK[6] = {};
+    probabilidadesK[0] = probabilidades[0];
+
+    for (int i = 1; i < 6; ++i) {
+        probabilidadesK[i] = probabilidadesK[i-1] + probabilidades[i];
     }
 
-    if (probabilidadCal < 0) {
-        probabilidadCal += 100;
-    }
-
-    if (probabilidadCal <= 5) {
+    if (probabilidadCal <= probabilidadesK[0]) {
         calificacion += generarCalificacionAleatoria(0, 20);
-    } else if (probabilidadCal <= 15) {
+    } else if (probabilidadCal <= probabilidadesK[1]) {
         calificacion += generarCalificacionAleatoria(20, 40);
-    } else if (probabilidadCal <= 30) {
+    } else if (probabilidadCal <= probabilidadesK[2]) {
         calificacion += generarCalificacionAleatoria(40, 55);
-    } else if (probabilidadCal <= 60) {
+    } else if (probabilidadCal <= probabilidadesK[3]) {
         calificacion += generarCalificacionAleatoria(55, 65);
-    } else if (probabilidadCal <= 80) {
+    } else if (probabilidadCal <= probabilidadesK[4]) {
         calificacion += generarCalificacionAleatoria(65, 85);
-    } else if (probabilidadCal <= 95) {
+    } else if (probabilidadCal <= probabilidadesK[5]) {
         calificacion += generarCalificacionAleatoria(85, 90);
     } else {
         calificacion += generarCalificacionAleatoria(90, 100);
     }
     
     // cout << "Probabilidad Dif: " << probabilidadDif << endl;
-    // cout << "Dificultad: " << float(dificultad)/10 << endl;
+    // cout << "Dificultad: " << dificultad << endl;
+    // Imprimir calificaciones filtradas
+    // cout << "probabilidades: ";
+    // for (int i = 0; i < 6; ++i) {
+    //     cout << probabilidades[i] << " ";
+    // }
+    // cout << endl;
+    // cout << "probabilidadesK: ";
+    // for (int i = 0; i < 6; ++i) {
+    //     cout << probabilidadesK[i] << " ";
+    // }
+    // cout << endl;
     // cout << "Probabilidad Cal: " << probabilidadCal << endl;
-    cout << "Calificacion: " << float(calificacion)/10 << endl;
+    // cout << "Calificacion: " << float(calificacion)/10 << endl;
     
     // Llenar array de calificaciones
     for (int i = 0; i < 7; ++i) {
